f7f869ac6e9e64d09a8c5068600475dcbce9df72
NoahRizdon <142844221+NoahRizdon@users.noreply.github.com>
2026-09-09 19:22:30 +0000

    Update

--- 000_Labs/006_doubles/base_code/starter.java
+++ 000_Labs/006_doubles/base_code/starter.java
@@ -8,7 +8,10 @@ import java.util.Scanner;
 class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		// the command window when you compile and run this program
+		Scanner alexP = new Scanner(System.in);
+	Double c;
+	int f = 60;
+	c = (f*((double)(5/9) - 32);
 	}
 }
