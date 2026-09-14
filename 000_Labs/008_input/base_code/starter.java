d71cdf08a0e75d7cf9f8b9fd3ab14577522b99bf
NoahRizdon <142844221+NoahRizdon@users.noreply.github.com>
2026-09-14 19:45:16 +0000

    update

--- 000_Labs/008_input/base_code/starter.java
+++ 000_Labs/008_input/base_code/starter.java
@@ -5,10 +5,22 @@
 
 import java.util.Scanner;
 
+import javax.lang.model.element.Name;
+
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+	Scanner sc = new Scanner(System.in);
+	System.out.println("What is your first name?");
+	String text = sc.nextLine();
+	System.out.println("What is your age?");
+	sc.nextLine();
+	System.out.println("What month were you born in?");
+	sc.nextLine();
+	System.out.println("What year were you born in?");
+	int number = sc.nextInt();
+	sc.nextLine();
+	System.out.println("If I have a dollar and two quarters, how much money do I have?");
+	double dollar = sc.nextDouble();
+
 	}
 }
