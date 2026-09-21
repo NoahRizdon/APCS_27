686d31d128efbed0d14131f3d0979dabadc229b8
NoahRizdon <142844221+NoahRizdon@users.noreply.github.com>
2026-09-21 20:13:34 +0000

    update

--- 102_Exercises/10_RandomRange/base_code/starter.java
+++ 102_Exercises/10_RandomRange/base_code/starter.java
@@ -1,6 +1,6 @@
 /*
- *	Author:
- *  Date:
+ *	Author: Noah Risdon
+ *  Date: 9/17/26
  *	Collaborator(s): 
 */
 
@@ -8,8 +8,24 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Please choose two integers for your range");
+	System.out.println("Integer one: ");
+	int int1 = sc.nextInt();
+	System.out.println("Integer two (Needs to be bigger than the first): ");	
+	int int2 = sc.nextInt();
+	System.out.println("Your range is: " + int1 + " to " + int2);
+	System.out.println("Here are 5 random numbers in your range");
+	int range = int2 - int1;
+	int intOne = ((int)(Math.random()*range) + int1);
+	int intTwo = ((int)(Math.random()*range) + int1);
+	int intThree = ((int)(Math.random()*range) + int1);
+	int intFour = ((int)(Math.random()*range) + int1);
+	int intFive = ((int)(Math.random()*range) + int1);
+	System.out.println(intOne);
+	System.out.println(intTwo);
+	System.out.println(intThree);
+	System.out.println(intFour);
+	System.out.println(intFive);
 	}
 }
