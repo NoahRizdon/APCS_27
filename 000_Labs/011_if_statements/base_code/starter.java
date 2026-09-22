2ce5817894bdbc45b5059ff331cf111c10c27441
NoahRizdon <142844221+NoahRizdon@users.noreply.github.com>
2026-09-22 19:45:11 +0000

    update

--- 000_Labs/011_if_statements/base_code/starter.java
+++ 000_Labs/011_if_statements/base_code/starter.java
@@ -1,14 +1,17 @@
 /*
- *	Author:  
- *  Date: 
+ *	Author:  Noah Risdon
+ *  Date: 9/21/2026
 */
 
 import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+int val1 = 1029756
+int val2 = 2347702
+boolean compariSon = val1 != val2
+if(compariSon){
+	System.out
+}
 	}
 }
